ab880206f134eae74afbc56c83d669b62a784884
40Sabrina <sabrinaaaaa@hotmail.com>
2024-02-19 18:18:00 +0100

    adding searchbar and button login/sell

---
+++ frontend/src/components/HeaderNav.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+import "./HeaderNav.css";
+
+function HeaderNav() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // const handleSearch = () => {
+  //   fetch(
+  //     `https://api`
+  //   )
+  //     .then((response) => response.json())
+  //     .then((response) => {
+  //       if (response.results) {
+  //         setSearchResults(response.results);
+  //         console.info("Search Results:", response.results);
+  //       } else {
+  //         setSearchResults([]);
+  //         console.error("No results found.");
+  //       }
+  //     })
+  //     .catch((err) => console.error(err));
+  // };
+
+  // useEffect(() => {
+  //   const delaySearch = setTimeout(() => {
+  //     handleSearch();
+  //   }, 100);
+  //   return () => clearTimeout(delaySearch);
+  // }, [searchQuery]);
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      // window.location.href = `/explorer/${searchQuery}`; */ url a définir */
+    }
+  };
+
+  return (
+    <div className="navbar-header">
+      <header className="Navbar-top">
+        <div className="searchbar-container">
+          <div>
+            <div className="menu-triangle">
+              <button className="menu-button" type="button">
+                Menu
+              </button>
+              <div className="icone-triangle" />
+            </div>
+            <ul className="scroll-menu">
+              {/* <Link to={`/explorer/`}> */}
+              <li className="Explorer">Explorer</li>
+              {/* </Link> */}
+              {/* <Link to={`/Catalogue/`}> */}
+              <li className="Catalogue">Catalogue</li>
+              {/* </Link> */}
+            </ul>
+          </div>
+          <div className="line" />
+          <div className="icone-loupe" />
+          <input
+            type="text"
+            placeholder="Recherche"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            autoComplete="on"
+            onKeyPress={handleKeyPress}
+          />
+        </div>
+        <div className="buttonHeader-container">
+          <button className="incription-login-button" type="button">
+            S'incrire | Se connecter
+          </button>
+          <button className="vendre-button" type="button">
+            Vends tes Mangas
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default HeaderNav;
