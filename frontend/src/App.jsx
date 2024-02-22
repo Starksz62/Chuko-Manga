e1d5eca10ce4fa5e87f718c0d374b8e99b76b7c6
Daniela <dtricolici12345@gmail.com>
2024-02-22 10:54:09 +0100

    CREERPROFIL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 // import { useEffect } from "react";
 // import axios from "axios";
-import logo from "./assets/logo.svg";
 
 import "./App.css";
+// eslint-disable-next-line import/no-named-as-default
+import CreerProfil from "./pages/CreerProfil";
 
 function App() {
   // Test to fetch advert cards
@@ -16,36 +17,7 @@ function App() {
   //   getAdvertCards();
   // }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React !</p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+  return <CreerProfil />;
 }
 
 export default App;
