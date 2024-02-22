bcf80028dc0c1b48c60fc3760d630ac19f277ddc
Maeva Fauvel <mae.fauvel@gmail.com>
2024-02-22 12:41:38 +0100

    cards component : props done + array with data in app component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 // import { useEffect } from "react";
-// import axios from "axios";
-import logo from "./assets/logo.svg";
-import Counter from "./components/Counter";
+// // import axios from "axios";
+// import logo from "./assets/logo.svg";
+// import Counter from "./components/Counter";
+import Cards from "./components/Cards";
 
 import "./App.css";
 
@@ -17,37 +18,160 @@ function App() {
   //   getAdvertCards();
   // }, []);
 
+  const annonces = [
+    {
+      image: "./src/assets/NarutoTome13.jpg",
+      title: "Naruto - Tome 13",
+      price: "5,00",
+      favorite: "./src/assets/heart.png",
+      condition: "Bon état",
+      imageUser: "./src/assets/orange.jpg",
+      nameUser: "Utilisateur",
+      star: "./src/assets/orange.jpg",
+      note: "5",
+      comment: "24",
+    },
+    {
+      image: "./src/assets/20centuryboys.jpg",
+      title: "20 century boys - Tome 1",
+      price: "3,50",
+      favorite: "./src/assets/heart.png",
+      condition: "Neuf",
+      imageUser: "./src/assets/orange.jpg",
+      nameUser: "Utilisateur2",
+      star: "./src/assets/orange.jpg",
+      note: "4,9",
+      comment: "136",
+    },
+    {
+      image: "./src/assets/AOT.jpg",
+      title: "L'attaque des titans - Tome 17",
+      price: "4,00",
+      favorite: "./src/assets/heart.png",
+      condition: "Usé",
+      imageUser: "./src/assets/orange.jpg",
+      nameUser: "Utilisateur3",
+      star: "./src/assets/orange.jpg",
+      note: "5",
+      comment: "21",
+    },
+    {
+      image: "./src/assets/berserk.jpg",
+      title: "Berserk - Tome 1",
+      price: "2,00",
+      favorite: "./src/assets/heart.png",
+      condition: "Bon état",
+      imageUser: "./src/assets/orange.jpg",
+      nameUser: "Utilisateur4",
+      star: "./src/assets/orange.jpg",
+      note: "4,8",
+      comment: "258",
+    },
+    {
+      image: "./src/assets/deathnote.jpg",
+      title: "Death Note - Tome 6",
+      price: "2,50",
+      favorite: "./src/assets/heart.png",
+      condition: "Bon état",
+      imageUser: "./src/assets/orange.jpg",
+      nameUser: "Utilisateur5",
+      star: "./src/assets/orange.jpg",
+      note: "4,9",
+      comment: "136",
+    },
+    {
+      image: "./src/assets/hunterxhunter.jpg",
+      title: "HunterxHunter - Tome 1",
+      price: "4,00",
+      favorite: "./src/assets/heart.png",
+      condition: "Neuf",
+      imageUser: "./src/assets/orange.jpg",
+      nameUser: "Utilisateur6",
+      star: "./src/assets/orange.jpg",
+      note: "4,9",
+      comment: "136",
+    },
+    {
+      image: "./src/assets/MHA.jpg",
+      title: "My Hero Academia - Tome 22",
+      price: "3,00",
+      favorite: "./src/assets/heart.png",
+      condition: "Très bon état",
+      imageUser: "./src/assets/orange.jpg",
+      nameUser: "Utilisateur7",
+      star: "./src/assets/orange.jpg",
+      note: "4,7",
+      comment: "76",
+    },
+    {
+      image: "./src/assets/nana.jpg",
+      title: "Nana - Tome 8",
+      price: "2,00",
+      favorite: "./src/assets/heart.png",
+      condition: "Bon état",
+      imageUser: "./src/assets/orange.jpg",
+      nameUser: "Utilisateur8",
+      star: "./src/assets/orange.jpg",
+      note: "4,9",
+      comment: "45",
+    },
+    {
+      image: "./src/assets/orangemanga.jpg",
+      title: "Orange - Tome 1",
+      price: "4,00",
+      favorite: "./src/assets/heart.png",
+      condition: "Neuf",
+      imageUser: "./src/assets/orange.jpg",
+      nameUser: "Utilisateur9",
+      star: "./src/assets/orange.jpg",
+      note: "4,8",
+      comment: "30",
+    },
+    {
+      image: "./src/assets/vinlandsaga.jpg",
+      title: "Vinland Saga - Tome 1",
+      price: "3,00",
+      favorite: "./src/assets/heart.png",
+      condition: "Bon état",
+      imageUser: "./src/assets/orange.jpg",
+      nameUser: "Utilisateur10",
+      star: "./src/assets/orange.jpg",
+      note: "4,6",
+      comment: "98",
+    },
+    {
+      image: "./src/assets/rabbitdoubt.jpg",
+      title: "Rabbit Doubt - Tome 1",
+      price: "2,50",
+      favorite: "./src/assets/heart.png",
+      condition: "Usé",
+      imageUser: "./src/assets/orange.jpg",
+      nameUser: "Utilisateur11",
+      star: "./src/assets/orange.jpg",
+      note: "4,9",
+      comment: "129",
+    },
+    {
+      image: "./src/assets/sailormoon.jpg",
+      title: "Sailor Moon - Tome 2",
+      price: "3,00",
+      favorite: "./src/assets/heart.png",
+      condition: "Bon état",
+      imageUser: "./src/assets/orange.jpg",
+      nameUser: "Utilisateur12",
+      star: "./src/assets/orange.jpg",
+      note: "4,4",
+      comment: "3",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React !</p>
-
-        <Counter />
-
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="main">
+      {annonces.map((annonce) => (
+        <div key={annonce.id} className="annonce-item">
+          <Cards data={annonce} />
+        </div>
+      ))}
     </div>
   );
 }
--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,28 +1,35 @@
+/* eslint-disable react/prop-types */
 import "./Cards.css";
-import Naruto from "../assets/NarutoTome13.jpg";
-import Favorite from "../assets/heart.png";
-import Orange from "../assets/orange.jpg";
+// import Naruto from "../assets/NarutoTome13.jpg";
+// import Favorite from "../assets/heart.png";
+// import Orange from "../assets/orange.jpg";
 
-function Cards() {
+function Cards({ data }) {
   return (
     <section className="card-content">
-      <img src={Naruto} alt="" className="card-image" />
-      <h2 className="card-title">Naruto Tome 34</h2>
+      <img src={data.image} alt={data.title} className="card-image" />
+      <h2 className="card-title">{data.title}</h2>
       <div className="card-price-section">
-        <p className="card-price">5,00€</p>
-        <img src={Favorite} alt="" className="card-favorite" />
+        <p className="card-price">{data.price}€</p>
+        <img
+          src={data.favorite}
+          alt="logo favorite"
+          className="card-favorite"
+        />
       </div>
-      <p className="card-condition">Bon état</p>
+      <p className="card-condition">{data.condition}</p>
       <div className="card-user-section">
         <div className="user">
-          <img src={Orange} alt="" className="card-user-photo" />
-          <p className="card-user-name">Utilisateur</p>
+          <img src={data.imageUser} alt="user" className="card-user-photo" />
+          <p className="card-user-name">{data.nameUser}</p>
         </div>
         <div className="note">
-          <img src={Orange} alt="" className="card-star" />
+          <img src={data.star} alt="logo star" className="card-star" />
           <div className="note-text">
-            <p className="card-evaluation">5</p>
-            <p className="card-number-feedback">(24)</p>
+            <p className="card-evaluation">
+              {data.note}
+              <span className="card-number-feedback">({data.comment})</span>
+            </p>
           </div>
         </div>
       </div>
