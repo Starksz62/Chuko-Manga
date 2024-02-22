a3faf5623349c0c0a29ff237d2a221f9520a6e53
40Sabrina <sabrinaaaaa@hotmail.com>
2024-02-22 07:29:19 +0100

    add average rating for the route getUserProfilById

--- backend/src/models/UsersManager.js
+++ backend/src/models/UsersManager.js
@@ -15,15 +15,14 @@ class UsersManager extends AbstractManager {
 
   async getUserProfilById(id) {
     const [rows] = await this.database.query(
-      `SELECT user.pseudo, user.email, user.phone, user.picture, address.country, feedback.rating
+      `SELECT user.pseudo, user.email, user.phone, user.picture, address.country, (SELECT AVG(feedback.rating) FROM feedback WHERE user_id = ?) AS average_rating
       FROM ${this.table} 
-      JOIN feedback on user.id = feedback.user_id
       JOIN address_has_user on user.id = address_has_user.user_id
       JOIN address on address_has_user.address_id = address.id
       WHERE address_has_user.user_id = ?
       ORDER BY address.id ASC
       LIMIT 1`,
-      [id]
+      [id, id]
     );
     return rows[0];
   }
