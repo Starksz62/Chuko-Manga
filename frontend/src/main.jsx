14480fbc7af083a19581dd87647a294e3afb6871
Lor92500 <149686206+Lor92500@users.noreply.github.com>
2024-02-22 11:15:30 +0100

    update fetch

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "manga/:mangaId",
+        path: "manga/:id",
         element: <MangaDetails />,
       },
     ],
--- frontend/src/pages/MangaDetails.jsx
+++ frontend/src/pages/MangaDetails.jsx
@@ -1,53 +1,55 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
+import React, { useState, useEffect } from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from "axios";
 
-// function MangaDetails() {
-//   const [manga, setManga] = useState(null);
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [error, setError] = useState(null);
+function MangaDetails() {
+  const [manga, setManga] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-//   useEffect(() => {
-//     axios
-//       .get("http://exemple.com/api/mangas/2")
-//       .then((response) => {
-//         setManga(response.data);
-//         setIsLoading(false);
-//       })
-//       .catch((error) => {
-//         console.error("Axios error:", error);
-//         setError(error.toString());
-//         setIsLoading(false);
-//       });
-//   }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost3000/api/mangas/id")
+      .then((response) => {
+        setManga(response.data);
+        setIsLoading(false);
+      })
+      // eslint-disable-next-line no-shadow
+      .catch((error) => {
+        console.error("Axios error:", error);
+        setError(error.toString());
+        setIsLoading(false);
+      });
+  }, []);
 
-//   if (isLoading) return <div>Chargement...</div>;
-//   if (error) return <div>Erreur : {error}</div>;
+  if (isLoading) return <div>Chargement...</div>;
+  if (error) return <div>Erreur : {error}</div>;
 
-//   return (
-//     <div>
-//       {manga && (
-//         <div>
-//           <h2>{manga.title}</h2>
-//           <p>
-//             <strong>Description :</strong> {manga.description}
-//           </p>
-//           <p>
-//             <strong>Auteur :</strong> {manga.author}
-//           </p>
-//           <p>
-//             <strong>Illustrateur :</strong> {manga.illustrator}
-//           </p>
-//           <p>
-//             <strong>Date de sortie :</strong>{" "}
-//             {new Date(manga.release_date).getFullYear()}
-//           </p>
-//           <p>
-//             <strong>Maison d'édition :</strong> {manga.publishing_house_id}
-//           </p>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      {manga && (
+        <div>
+          <h2>{manga.title}</h2>
+          <p>
+            <strong>Description :</strong> {manga.description}
+          </p>
+          <p>
+            <strong>Auteur :</strong> {manga.author}
+          </p>
+          <p>
+            <strong>Illustrateur :</strong> {manga.illustrator}
+          </p>
+          <p>
+            <strong>Date de sortie :</strong>{" "}
+            {new Date(manga.release_date).getFullYear()}
+          </p>
+          <p>
+            <strong>Maison d'édition :</strong> {manga.publishing_house_id}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
 
-// export default MangaDetails;
+export default MangaDetails;
