360bdbaa5d0e40d84ae4304997e2ea4d217015c0
Lor92500 <149686206+Lor92500@users.noreply.github.com>
2024-02-23 09:55:35 +0100

    updated navBar css

--- frontend/src/components/LeftNavbar.jsx
+++ frontend/src/components/LeftNavbar.jsx
@@ -13,16 +13,49 @@ import "./LeftNavbar.css";
 
 function LeftNavbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [showFilters, setShowFilters] = useState(false);
+  const [currentFilter, setCurrentFilter] = useState(null);
 
   // Étend la sidebar lors du survol
   const handleMouseEnter = () => {
     setIsMenuOpen(true);
   };
 
-  // Réduit la sidebar lorsque la souris quitte la zone
   const handleMouseLeave = () => {
+    if (!showFilters) {
+      setIsMenuOpen(false);
+    }
     setIsMenuOpen(false);
   };
+
+  // const toggleFilters = () => {
+  //   setShowFilters(!showFilters);
+  //   if (!showFilters) {
+  //     setIsMenuOpen(true);
+  //   }
+  // };
+
+  const toggleFilters = () => {
+    const newShowFilters = !showFilters;
+    setShowFilters(newShowFilters);
+
+    if (newShowFilters) {
+      setIsMenuOpen(true);
+    }
+  };
+
+  const handleFilterSelection = (filter) => {
+    setCurrentFilter((prevFilter) => (prevFilter === filter ? null : filter));
+  };
+
+  const genreOptions = [
+    "Shonen",
+    "Shojo",
+    "Seinen",
+    "Josei",
+    "Kodomo",
+    "Isekai",
+  ];
 
   return (
     <div
@@ -59,10 +92,62 @@ function LeftNavbar() {
             <img src={SortIcon} alt="Tri" />
             <span>{isMenuOpen && "Tri"}</span>
           </li>
-          <li>
+          <button
+            type="button"
+            onClick={toggleFilters}
+            className="filter-category"
+          >
             <img src={FilterIcon} alt="Filtre" />
-            <span>{isMenuOpen && "Filtre"}</span>
-          </li>
+            <span className="filter-text">Filtre</span>
+          </button>
+          {showFilters && (
+            <div className="filter-dropdown">
+              <ul>
+                <li>
+                  <button
+                    type="button"
+                    className={`genres ${
+                      currentFilter === "Genres" ? "active" : ""
+                    }`}
+                    onClick={() => handleFilterSelection("Genres")}
+                  >
+                    Genres
+                  </button>
+                  {currentFilter === "Genres" && (
+                    <ul>
+                      {genreOptions.map((genre) => (
+                        <button
+                          type="button"
+                          key={genre}
+                          onClick={() => handleFilterSelection(genre)}
+                        >
+                          {genre}
+                        </button>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+                <li>
+                  <button
+                    type="button"
+                    className="condition"
+                    onClick={() => handleFilterSelection("Etat")}
+                  >
+                    Etat
+                  </button>
+                </li>
+                <li>
+                  <button
+                    type="button"
+                    className="price"
+                    onClick={() => handleFilterSelection("Prix")}
+                  >
+                    Prix
+                  </button>
+                </li>
+              </ul>
+            </div>
+          )}
         </ul>
       </div>
     </div>
