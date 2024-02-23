dd8e5789bf9878bae65fdb5a9585ccc6b8cc6645
Daniela <dtricolici12345@gmail.com>
2024-02-23 10:07:02 +0100

    finished 004D

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,5 @@
-// import { useEffect } from "react";
-// import axios from "axios";
-
 import "./App.css";
-// eslint-disable-next-line import/no-named-as-default
-import CreerProfil from "./pages/CreerProfil";
 
-function App() {
-  // Test to fetch advert cards
-  // const getAdvertCards = () => {
-  //   axios.get("http://localhost:3310/api/display-adverts/1").then((res) => {
-  //     console.info(res.data);
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   getAdvertCards();
-  // }, []);
-
-  return <CreerProfil />;
-}
+function App() {}
 
 export default App;
