a1e15893d10348f9efac38dd81b2c4f318c06399
40Sabrina <sabrinaaaaa@hotmail.com>
2024-02-23 10:34:17 +0100

    update app frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,8 @@
 import HeaderNav from "./components/HeaderNav";
-// import { useEffect } from "react";
-// import axios from "axios";
 
 import "./App.css";
 
 function App() {
-<<<<<<< HEAD
-=======
-  // Test to fetch advert cards
-  // const getAdvertCards = () => {
-  //   axios.get("http://localhost:3310/api/display-adverts/1").then((res) => {
-  //     console.info(res.data);
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   getAdvertCards();
-  // }, []);
-
->>>>>>> dev
   return (
     <div className="HeaderNav">
       <HeaderNav />
