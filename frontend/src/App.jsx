499aa40b9a5eed48b5e686cd6a21b04001942714
40Sabrina <sabrinaaaaa@hotmail.com>
2024-02-27 16:33:16 +0100

    add onglet mes commentaires

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,6 +25,8 @@ function App() {
   //   fetchData();
   // }, []);
   const [articles, setArticles] = useState();
+  const [evaluations, setEvaluations] = useState([]);
+  const [ongletActif, setongletActif] = useState("Articles");
 
   useEffect(() => {
     fetch(`http://localhost:3310/api/display-adverts-byseller/1`)
@@ -35,6 +37,20 @@ function App() {
       });
   }, []);
 
+  useEffect(() => {
+    fetch(`http://localhost:3310/api/user-profil-com/1`)
+      .then((res) => res.json())
+      .then((data) => {
+        // Ajouter une propriété id à chaque objet
+        const evaluationsWithIds = [data].map((item, index) => ({
+          ...item,
+          id: index + 1, // Utiliser l'index + 1 comme ID
+        }));
+        console.info("commentairesTableau:", evaluationsWithIds);
+        setEvaluations(evaluationsWithIds);
+      });
+  }, []);
+
   return (
     <>
       {/* <LeftNavbar />
@@ -42,33 +58,69 @@ function App() {
       <Outlet context={[data]} />; */}
       <div className="containerOnglets">
         <div className="containerButton">
-          <button type="button" className="buttonOnglet">
+          <button
+            type="button"
+            className="buttonOnglet"
+            onClick={() => setongletActif("Articles")}
+          >
             Articles
           </button>
-          <button type="button" className="buttonOnglet">
+          <button
+            type="button"
+            className="buttonOnglet"
+            onClick={() => setongletActif("Mes évaluations")}
+          >
             Mes évaluations
           </button>
-          <button type="button" className="buttonOnglet">
+          <button
+            type="button"
+            className="buttonOnglet"
+            onClick={() => setongletActif("Mon historique d'achat")}
+          >
             Mon historique d'achat
           </button>
         </div>
-        <div className="containerArticles">
-          {articles?.map((article) => (
-            <div key={article.id}>
-              <li className="card">
-                <div>
-                  <img
-                    className="image_path"
-                    src={`${article.image_path}`}
-                    alt="image_article_seller"
-                  />
-                  <p className="title_search_manga">{`${article.title_search_manga}`}</p>
-                  <p className="price">{`${article.price}`} €</p>
-                  <p className="name_condition">{`${article.name_condition}`}</p>
+        <div className="containerInformations">
+          <div className="containerArticles">
+            {ongletActif === "Articles" &&
+              articles?.map((article) => (
+                <div key={article.id}>
+                  <li className="card">
+                    <div>
+                      <img
+                        className="image_path"
+                        src={`${article.image_path}`}
+                        alt="image_article_seller"
+                      />
+                      <p className="title_search_manga">{`${article.title_search_manga}`}</p>
+                      <p className="price">{`${article.price}`} €</p>
+                      <p className="name_condition">{`${article.name_condition}`}</p>
+                    </div>
+                  </li>
                 </div>
-              </li>
-            </div>
-          ))}
+              ))}
+          </div>
+          <div className="containerEvaluations">
+            {ongletActif === "Mes évaluations" &&
+              evaluations?.map((evaluation) => (
+                <div key={evaluation.id}>
+                  <li className="cardCom">
+                    <div>
+                      <p className="average_rating">{`${evaluation.average_rating}`}</p>
+                      <img
+                        className="picture_buyer"
+                        src={`${evaluation.picture_buyer}`}
+                        alt="image_buyer"
+                      />
+                      <p className="created_on">{`${evaluation.created_on.split("T")[0]}`}</p>
+                      <p className="speudo_buyer">{`${evaluation.pseudo}`}</p>
+                      <p className="rating">{`${evaluation.rating}`}</p>
+                      <p className="com">{`${evaluation.comment}`}</p>
+                    </div>
+                  </li>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </>
