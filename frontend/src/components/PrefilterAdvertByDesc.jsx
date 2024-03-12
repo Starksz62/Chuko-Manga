7d89333f36d153fb13ed1ffd7993fc615b7fea98
Gwenael <gwenael.begot@outlook.fr>
2024-03-12 14:43:09 +0100

    add scroll in the prefiltre

--- frontend/src/components/PrefilterAdvertByDesc.jsx
+++ frontend/src/components/PrefilterAdvertByDesc.jsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+
 import AdvertCard from "./AdvertCard";
 import "./PrefilterAdvertByDesc.css";
 
-function FilteredadvertsCard() {
+function PrefilterAdvertByDesc() {
   const [, setAdverts] = useState([]);
   const [filteredAdverts, setFilteredAdverts] = useState([]);
+  const containerRef = useRef(null);
 
   useEffect(() => {
     fetch("http://localhost:3310/api/unique-adverts-date-desc")
@@ -25,30 +27,55 @@ function FilteredadvertsCard() {
           error
         );
       });
+
+    // Gestion du défilement à la molette de la souris
+    const handleMouseWheel = (e) => {
+      containerRef.current.scrollLeft += e.deltaY;
+      e.preventDefault();
+    };
+
+    // Ajout de l'écouteur d'événements pour la molette de la souris
+    containerRef.current.addEventListener("wheel", handleMouseWheel);
+
+    // Nettoyage de l'écouteur d'événements lors du démontage du composant
+    return () => {
+      if (containerRef.current) {
+        // Vérifie si containerRef.current est différent de null
+        containerRef.current.removeEventListener("wheel", handleMouseWheel);
+      }
+    };
   }, []);
 
   return (
-    <div className="App">
+    <div className="prefilter-section">
       <h1>Explorer les derniers tomes ajoutés :</h1>
-      <div className="FilterByDate">
-        {filteredAdverts.length > 0 ? (
-          filteredAdverts
-            .slice(0, 3)
-            .map((advert) => <AdvertCard key={advert.id} advert={advert} />)
-        ) : (
-          <p>Chargement en cours...</p>
-        )}
-        <Link to="/explore">
-          <button type="button" className="bntSeeAllTomes">
-            <div className="textBtn">
-              {" "}
-              Voir tous <br /> les tomes
-            </div>
-          </button>
-        </Link>
+      <div className="FilterByDateDescWrapper">
+        <div className="FilterByDateDesc" ref={containerRef}>
+          <div className="filteredAdverts">
+            {filteredAdverts.length > 0 ? (
+              filteredAdverts.map((advert) => (
+                <div key={advert.id} className="AdvertCard">
+                  <AdvertCard advert={advert} />
+                </div>
+              ))
+            ) : (
+              <p>Chargement en cours...</p>
+            )}
+          </div>
+        </div>
+        <div className="seeAllTomesButtonWrapper">
+          <Link to="/explore">
+            <button type="button" className="bntSeeAllTomesDesc">
+              <div className="textBtnDesc">
+                {" "}
+                Voir tous <br /> les tomes
+              </div>
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
 }
 
-export default FilteredadvertsCard;
+export default PrefilterAdvertByDesc;
