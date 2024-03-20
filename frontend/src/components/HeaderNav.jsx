d051c24dcf4d18e099b83c6120c263c1a208701b
Paul-Lac <plnlacroix@gmail.com>
2024-03-20 22:14:44 +0100

    Make background non-scrollable when modals are open

--- frontend/src/components/HeaderNav.jsx
+++ frontend/src/components/HeaderNav.jsx
@@ -14,6 +14,11 @@ function HeaderNav() {
 
   const handleClickOpen = () => {
     setOpen(!open);
+    if (!open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
   };
 
   return (
