2ec512b761979b42c43fe73347da8802f97a6d0a
Maeva Fauvel <mae.fauvel@gmail.com>
2024-03-25 15:02:26 +0100

    fetch added for recherche section

--- frontend/src/components/RappelRecherche.jsx
+++ frontend/src/components/RappelRecherche.jsx
@@ -1,25 +1,45 @@
+import { useEffect, useState } from "react";
 import "./RappelRecherche.css";
 import SearchBar from "./SearchBar";
 
 function RappelRecherche() {
+  const [mangas, setMangas] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3310/api/manga/catalog")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error HTTP, status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setMangas(data);
+      })
+      .catch((error) => {
+        console.error("An error occurred while fetching data:", error);
+      });
+  }, []);
+
   return (
     <section className="rappel-recherche-container">
       <h3 className="title-rappel-recherche">
         Recherche le tome qu'il te manque :
       </h3>
       <SearchBar />
-      <br />
-      <br />
       <h3 className="title-rappel-recherche">Suggestions de recherche :</h3>
       {/* Faire un axios pour récupérer des noms de mangas du catalogue */}
       <ul className="list-suggestion">
-        <li className="sugg">One piece</li>
-        <li className="sugg">Naruto</li>
+        {mangas.map((manga) => (
+          <li key={manga.id} className="sugg">
+            {manga.title}
+          </li>
+        ))}
+        {/* <li className="sugg">Naruto</li>
         <li className="sugg">Hajime no Ippo</li>
         <li className="sugg">Shangri-la Frontier</li>
         <li className="sugg">Berserk</li>
         <li className="sugg">Blue lock</li>
-        <li className="sugg">Chi</li>
+        <li className="sugg">Chi</li> */}
       </ul>
     </section>
   );
